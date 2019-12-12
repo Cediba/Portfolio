b2703476c94b4fc5dc377169f9f668423154e049
Cedric <cedric.balmer@gmx.ch>
2019-12-12 15:50:40 -0800

    set up react for my portfolio

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react- router-dom';
+import Footer from "../Footer/Footer";
+import Contact from "../Contact/Contact"
+import './App.css';
+import "./reset.css";
+import NotFound from "../NotFound/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* <Header/> */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+
+export default App;
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+      <div>Here is 404 page</div>
+  );
+};
+
+export default NotFound;

aed40d05621efdc7d16a941bbc399c7f9689f2bb
Cedric <cedric.balmer@gmx.ch>
2019-12-16 12:32:29 -0800

    started top page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from "../Footer/Footer";
 import Header from '../Header/NavBar';
 import Home from '../Home/Home';
-import './App.css';
 import "./reset.css";
+import './App.css';
 import NotFound from "../NotFound/NotFound";
 import Contact from '../Contact/Contact';
 import Certificates from '../Certificates/Certificates';
+// eslint-disable-next-line no-unused-expressions
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet"/>
 
 const App = () => {
   return (
--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Top.scss';
 import Logo from '../../assets/images/logo1.png';
-
+import persImg from '../../assets/images/pers-img.jpg';
 
 const Top = () => {
     return (
         <div className='wrapper'>
 
-            <div className='logo'><img src={Logo} alt="Logo" width="100" height="70"/></div>
+            <div className='logo'>
+                <img src={Logo} alt="Logo" width="100"/>
+                </div>
 
-            <div className='pers-img'></div>
+            <div className='pers-slogan'>
+                Be yourself
+            </div>
 
-            <div className='pers-slogan'></div>
+            <div className='pers-img'>
+                <img class="image-circle" src={persImg} width="250" height="250" alt="persImg"/>
+            </div>
 
-            <div className='pers-name'>Cedric Balmer</div>
+            <div className='pers-name'>
+                <h1><span id="First-name">Cedric</span><span id="Second-name">Balmer</span></h1>
+                    <span id="subtitle">Webdeveloper</span>
+            </div>
 
             <div className='social-media'></div>
 

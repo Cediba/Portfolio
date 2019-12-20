19dd299a625c43a87f9158928981487bd476ca46
Cedric <cedric.balmer@gmx.ch>
2019-12-19 16:03:22 -0800

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Footer.scss';
+import '../Social-media/SocialButtonsPage';
+import SocialButtonsPage from '../Social-media/SocialButtonsPage';
 
 const Footer = () => {
     return (
       <footer>
-        <div>
-          Footer component
-        </div>
+          <div className='footer-person-name'>
+            <h2>Cedric Balmer</h2><h3>Webdeveloper</h3>
+          </div>
+          <div className='footer-socialMedia'>
+            <SocialButtonsPage/>
+          </div>
       </footer>
     )
 }
